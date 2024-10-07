728d14f453639c2fb1a084d450abe7bfff91ac77
MichalKalke <michalkalke@gmail.com>
2024-10-07 14:21:58 +0200

    Local test

--- test/btp/tf/variables.tf
+++ test/btp/tf/variables.tf
@@ -60,3 +60,9 @@ variable "BTP_KYMA_REGION" {
   description = "Kyma region"
   default     = "westeurope"
 }
+
+variable "BTP_PROVIDER_SUBACCOUNT_ID" {
+  type        = string
+  description = "Subaccount ID"
+  default     = null
+}
